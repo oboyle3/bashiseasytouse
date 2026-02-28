8100ec820205a2bb3a06f88a03fcf82d40534d99
oboyle3 <oboyle3patrick@gmail.com>
2026-02-28 10:20:32 -0500

    added basics.sh that will display some info

---
+++ basics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "===================="
+echo " Bash Script Basics "
+echo "===================="
+echo " User $(whoami)"
+echo "Current directory $(pwd)"
+echo ""
+echo "files inthis directory "
+ls -ltr
+echo "Hello, Bash World!"
+echo "My name is oboyle3"
+echo "Todays date is : "
+date
+
